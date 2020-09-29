405494567f7fec73199dc2809c90aac2febb590c
Raymond de Jong <raymonddejong@gmail.com>
2020-09-29 14:14:13 +0200

    Initial commit

---
+++ VRF-Lite/outputs.tf
@@ -0,0 +1,3 @@
+# output "tier0_gw_id"{
+#     value = nsxt_policy_tier0_gateway.vrf_tier0_gw.id
+# }
---
+++ VRF-Lite/variables.tf
@@ -0,0 +1,55 @@
+# Variables
+# NSX Manager
+variable "nsx_manager" {
+    default = "10.29.12.206"
+}
+
+# Username & Password for NSX-T Manager
+  variable "username" {
+  default = "admin"
+}
+
+variable "password" {
+    default = "VMware1!rdejong"
+}
+
+# Enter Edge Nodes Display Name. Required for external interfaces.
+variable "edge_node_1" {
+   default = "edge-01"
+}
+variable "edge_node_2" {
+   default = "edge-02"
+}
+
+# Segment Names
+variable "nsx_segment_web" {
+    default = "VM-Segment-Web"
+}
+variable "nsx_segment_app" {
+    default = "VM-Segment-App"
+}
+
+variable "nsx_segment_db" {
+    default = "VM-Segment-DB"
+}
+
+# Security Group names. 
+variable "nsx_group_web" {
+    default = "Web Servers"
+}
+
+variable "nsx_group_app" {
+    default = "App Servers"
+}
+
+variable "nsx_group_db" {
+    default = "DB Servers"
+}
+
+variable "nsx_group_blue" {
+    default = "Blue Application"
+}
+
+variable "nsx_group_red" {
+    default = "Red Application"
+}
